982b11d35141944c7506402807e8da7ed0d3a185
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-13 13:08:17 -0300

    conecting stacklist

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import stacks from '../data/stacks.json'
+import { setStack } from '../actions'
 
 class StackList extends Component {
+  handleClick = stack => {
+    const { setStack } = this.props
+
+    setStack(stack)
+  }
+
   render() {
     return (
       <div>
         {stacks.map(stack => (
-          <Link key={stack.id} to='/stack'>
+          <Link key={stack.id} to='/stack' onClick={this.handleClick}>
             <h4>{stack.title}</h4>
           </Link>
         ))}
@@ -16,4 +24,9 @@ class StackList extends Component {
   }
 }
 
-export default StackList
+const connectComponent = connect(
+  null,
+  { setStack },
+)(StackList)
+
+export default connectComponent
