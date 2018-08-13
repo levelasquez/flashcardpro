bc16fd123f08b2791a328358aaeb9fefde4e143f
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-13 12:10:48 -0300

    setting routes and links

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import stacks from '../data/stacks.json'
 
 class StackList extends Component {
@@ -6,7 +7,9 @@ class StackList extends Component {
     return (
       <div>
         {stacks.map(stack => (
-          <h4 key={stack.id}>{stack.title}</h4>
+          <Link key={stack.id} to='/stack'>
+            <h4>{stack.title}</h4>
+          </Link>
         ))}
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './components/App'
+import Stack from './components/Stack'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/stack' component={Stack} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
