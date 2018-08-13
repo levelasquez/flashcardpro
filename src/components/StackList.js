60fa23eb706abf191477b8003f1d0a8d9975d4b6
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-13 11:59:59 -0300

    loading the stacklist

---
+++ src/components/StackList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import stacks from '../data/stacks.json'
+
+class StackList extends Component {
+  render() {
+    return (
+      <div>
+        {stacks.map(stack => (
+          <h4 key={stack.id}>{stack.title}</h4>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default StackList
