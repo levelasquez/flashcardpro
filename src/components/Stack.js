6d0c41b4bcd21943886fd647263ced007a469183
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-14 10:43:20 -0300

    stateless functional components

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -3,24 +3,18 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Card from './Card'
 
-export class Stack extends Component {
-  render() {
-    const { stack: { title, cards = [] } } = this.props
-
-    return (
-      <div>
-        <Link className='link-home' to='/'>
-          <h4>Home</h4>
-        </Link>
-        <h3>{title}</h3>
-        <br />
-        {cards.map(card => (
-          <Card key={card.id} card={card} />
-        ))}
-      </div>
-    )
-  }
-}
+export const Stack = ({ stack: { title, cards } }) => (
+  <div>
+    <Link className='link-home' to='/'>
+      <h4>Home</h4>
+    </Link>
+    <h3>{title}</h3>
+    <br />
+    {cards.map(card => (
+      <Card key={card.id} card={card} />
+    ))}
+  </div>
+)
 
 const mapStateToProps = ({ stack }) => ({ stack })
 
